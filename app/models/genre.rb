3d23bdc6e8f709adb3fb0d76d62fa691bb0a28ba
gatsuonrails <gatsuonrails@gmail.com>
2020-11-21 13:20:07 +0900

    icon added

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -3,4 +3,5 @@ class Genre < ApplicationRecord
   has_many :genre_recommend_tags, dependent: :destroy
   accepts_nested_attributes_for :genre_recommend_tags, allow_destroy: true
   has_one_attached :image #active_starage
+  has_one_attached :icon #active_starage
 end
